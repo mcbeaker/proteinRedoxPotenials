be91a42ee171d5dc20cce40161ef4a21594816bc
kmcbeaker <kenneth.mcguinness@gmail.com>
2023-01-24 22:41:14 +0000

    in the middle of adding react-redux-fb

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,21 @@
 
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore' // <- needed if using firestore
+import { combineReducers, compose } from 'redux'
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  ReactReduxFirebaseProvider,
+  firebaseReducer
+} from 'react-redux-firebase'
+import { createFirestoreInstance, firestoreReducer } from 'redux-firestore' // <- needed if using firestore
+
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -17,8 +32,33 @@ const firebaseConfig = {
     appId: "1:1070866434741:web:9b83f77dfe9f2ce3bb8cfc",
     measurementId: "G-EMFPX708HR"
   };
+
+  // react-redux-firebase config
+const rrfConfig = {
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  initializeApp(firebaseConfig);
+
+  firebase.firestore() // <- needed if using firestore
+
+  const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer // <- needed if using firestore
+  })
+
+  // Create store with reducers and initial state
+const initialState = {}
+const store = configureStore(rootReducer, initialState)
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance // <- needed if using firestore
+}
+
   // Reference to your Firestore database
   export default getFirestore();
   
