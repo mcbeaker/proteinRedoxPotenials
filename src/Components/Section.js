3c500fe6e25f06b871dc1e018e7c4e19d36f7242
kmcbeaker <kenneth.mcguinness@gmail.com>
2023-01-31 20:18:42 +0000

    added single-page, memoization

---
+++ src/Components/Section.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Section({ Component, id }) {
+  return (
+
+    <div className="section">
+      <div className="section-content" id={id}>
+        <Component />  
+      </div>
+      </div>
+    
+  );
+}
